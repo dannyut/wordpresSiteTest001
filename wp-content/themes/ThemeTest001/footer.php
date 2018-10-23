faceb9705993a5f1e594ed401f35f300a52654c6
dannyut <dannyv40@gmail.com>
2018-10-22 22:04:11 -0700

    fix broken links & added simple file struction for simple theme test

---
+++ wp-content/themes/ThemeTest001/footer.php
@@ -0,0 +1,3 @@
+  <footer> <p>This is a test footer #001 (footer.php)</p> </footer>
+  </body>
+</html>
---
+++ wp-content/themes/ThemeTest001/header.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html>
+  <head>
+  	<meta charset="utf-8">
+  	<title> theme 001 (header.php) </title>
+  </head>
+  <body>
