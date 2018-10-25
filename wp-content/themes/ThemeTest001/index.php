9a68e82e3fbcf2712b8e8fffcd6a24b8a395a768
dannyut <dannyv40@gmail.com>
2018-10-24 21:05:21 -0700

    added posts loop

--- wp-content/themes/ThemeTest001/index.php
+++ wp-content/themes/ThemeTest001/index.php
@@ -1,3 +1,26 @@
 <?php get_header(); ?>
-<h1>This is my test index #001 (index.php) </h1>
+
+	
+	<?php
+
+		//echo "start if...";
+
+		if( have_posts() ):
+
+			//echo "start while...";
+			while( have_posts() ): the_post(); ?>
+
+
+
+				<h3><?php the_title(); ?></h3> 
+
+				<p><?php the_content(); ?></p> 
+
+			<?php endwhile;
+			
+		endif;
+
+	?>
+
+
 <?php get_footer(); ?>
