ef7cbad1371de0981bc9b17a1eee3e809463c384
dannyut <dannyv40@gmail.com>
2018-10-24 21:18:32 -0700

    custom templates

--- wp-content/themes/ThemeTest001/page-notitle.php
+++ wp-content/themes/ThemeTest001/page-notitle.php
@@ -1,4 +1,9 @@
-<?php get_header(); ?>
+<?php 
+/*
+	Template Name: Page No Title
+*/
+
+get_header(); ?>
 
 	
 	<?php
@@ -6,10 +11,6 @@
 		if( have_posts() ):
 
 			while( have_posts() ): the_post(); ?>
-
-
-
-				<h3><?php the_title(); ?></h3> 
 
 				<p><?php the_content(); ?></p> 
 
